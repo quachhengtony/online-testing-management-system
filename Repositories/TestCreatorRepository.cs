99ef4e3451509d212b25b73791faf59647bda369
TonyInfinity <quachhengtony@gmail.com>
2023-02-26 14:55:17 +0700

    add TestCreatorRepository for reference

---
+++ Repositories/TestCreatorRepository.cs
@@ -0,0 +1,43 @@
+﻿using BusinessObjects.Models;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    internal class TestCreatorRepository : ITestCreatorRepository
+    {
+        public void Create(TestCreator t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TestCreator t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TestCreator> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TestCreator GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TestCreator> GetByNameOrEmail(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TestCreator t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
