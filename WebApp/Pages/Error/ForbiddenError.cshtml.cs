5db9dc860f9bef8ccab2bd6ff428b7cbcbf9efcc
thanhpham0412 <ptt0412@gmail.com>
2023-03-18 10:47:12 +0700

    add forbidden error page

---
+++ WebApp/Pages/Error/ForbiddenError.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Error
+{
+    public class ForbiddenErrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
